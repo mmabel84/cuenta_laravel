efe164912d29a6fe72526a892844785df239936c
ADVANS\mmabel <mmabel@advans.mx>
2017-07-10 18:33:48 -0500

    cambiando color de vistas

--- cuenta/resources/views/backups.blade.php
+++ cuenta/resources/views/backups.blade.php
@@ -29,7 +29,7 @@
 		                  </div>
 		                  
 		                  <div class="form-group">
-		                  <button type="button" onclick="location.href = '{{ URL::to('backups/create') }}';" class="btn btn-round btn-primary" style="color:#FFFFFF; background-color:#005c99; "><b>Nuevo respaldo</b></button>
+		                  <button type="button" onclick="location.href = '{{ URL::to('backups/create') }}';" class="btn btn-round btn-primary" style="color:#FFFFFF; background-color:#486d91; "><b>Nuevo respaldo</b></button>
 		                  </div>
 
 		                  <br/>
@@ -51,7 +51,7 @@
 		                    
 		                    <table id="datatable-buttons" class="table table-striped table-bordered">
 		                      <thead>
-		                        <tr style="color:#FFFFFF; background-color:#005c99 ">
+		                        <tr style="color:#FFFFFF; background-color:#486d91 ">
 		                          <th>Empresa</th>
 		                          <th>Aplicación</th>
 		                          <th>Fecha</th>
