dcc752a9208f2825070c95c2563882b092fa381e
ADVANS\mmabel <mmabel@advans.mx>
2017-08-17 12:46:11 -0500

    quitando el link de descarga

--- cuenta/resources/views/backups.blade.php
+++ cuenta/resources/views/backups.blade.php
@@ -82,12 +82,12 @@
 										{{ Form::close() }}
 										@endpermission
 
-										&nbsp;
+										<!--&nbsp;
 										<div class="btn-group">
 
 		                          			<a href="{{ route('downlback',['bdid'=>$back->id]) }}" id="{{ $back->id }}" class="btn btn-xs" data-placement="left" title="Descargar respaldo" style=" color:#053666; background-color:#FFFFFF;"><i class="fa fa-download fa-3x"></i> </a>
 
-			                          	</div>
+			                          	</div>-->
 			                          	&nbsp;
 										<div class="btn-group">
 
