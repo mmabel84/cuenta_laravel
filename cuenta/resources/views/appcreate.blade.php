40058afda3ab0bfaac2e8f95836eb6e0ab6530b7
ADVANS\mmabel <mmabel@advans.mx>
2017-08-18 13:32:41 -0500

    convirtiendo a number campos de megas en vista de inst

--- cuenta/resources/views/appcreate.blade.php
+++ cuenta/resources/views/appcreate.blade.php
@@ -236,7 +236,7 @@
            
            if (cantDisp)
            {
-            if (cantAsign > cantDisp)
+            if (Number(cantAsign) > Number(cantDisp))
             {
               return false;
             }
