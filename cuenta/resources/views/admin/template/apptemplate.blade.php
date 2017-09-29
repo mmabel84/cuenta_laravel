0919b65ba4e04f332957a75645316e4ebea83b06
ADVANS\mmabel <mmabel@advans.mx>
2017-09-29 12:38:25 -0500

    subiendo favicon

--- cuenta/resources/views/admin/template/apptemplate.blade.php
+++ cuenta/resources/views/admin/template/apptemplate.blade.php
@@ -6,6 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}" />
+        <link type="image/x-icon" href="{{asset('favicon.ico')}}" rel="shortcut icon">
         <title>@yield('app_title')</title>
         @section('app_css')
             <!-- Fonts -->
