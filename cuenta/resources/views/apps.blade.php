99fe2eabe4dcd0298dd929096d05e39078eedb22
ADVANS\mmabel <mmabel@advans.mx>
2017-11-06 16:43:32 -0600

    reajustando tabla de soluciones

--- cuenta/resources/views/apps.blade.php
+++ cuenta/resources/views/apps.blade.php
@@ -71,10 +71,10 @@
 		                      <tbody>
 		                      @foreach ($apps as $a)
 		                        <tr>
-		                          <td width="5%">{{$a->aplicacion->app_nom}}</td>
+		                          <td width="5%" bgcolor="@if ($a->uso == 'Prueba') #81BEF7 @else #81F7BE @endif">{{$a->aplicacion->app_nom}}</td>
 		                          <td width="25%">{{$a->empresa ? $a->empresa->empr_nom: ''}}</td>
 		                          <td width="5%">{{$a->empresa ? $a->empresa->empr_rfc: ''}}</td>
-		                          <td width="2%" bgcolor="@if ($a->uso == 'Prueba') #81BEF7 @else #81F7BE @endif">{{$a->bdapp_gigdisp}}</td>
+		                          <td width="2%">{{$a->bdapp_gigdisp}}</td>
 		                          <td width="5%">{{$a->bdapp_imap_email}}</td>
 		                          <td width="28%">
 			                          <div class="btn-group">
