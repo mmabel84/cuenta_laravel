063b9ee4eb249852ef2a434aca0ec575fe4467b3
ADVANS\mmabel <mmabel@advans.mx>
2017-11-06 13:27:51 -0600

    subiendo liberacion e incremento de meg en solucion

--- cuenta/resources/views/apps.blade.php
+++ cuenta/resources/views/apps.blade.php
@@ -309,8 +309,6 @@
 				                          						 <div class="item form-group col-md-12 col-sm-12 col-xs-12">
 											                        <input id="cant_mg{{ $a->id }}" class="form-control has-feedback-left" name="cant_mg" type="number" title="Cantidad de Megas" required>
 											                        <span class="fa fa-pie-chart form-control-feedback left" aria-hidden="true"></span>
-
-											                        <button id="btnmg{{$a->id}}" type="button" class="btn btn-primary" onclick="modificarMegas({{$a->id}});">Ejecutar</button>
 											                     </div>
 											                      <br>	
 		                          								  <br>
