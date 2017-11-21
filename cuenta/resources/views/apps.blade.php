2ed6e26800972dfb5f733e91886c71ffe74ebbbc
ADVANS\mmabel <mmabel@advans.mx>
2017-11-21 13:33:04 -0600

    cambiando clase de tabla bitacora en modal de app

--- cuenta/resources/views/apps.blade.php
+++ cuenta/resources/views/apps.blade.php
@@ -184,6 +184,7 @@
 			                        							<div class="container">
 			                        							<div class="row">
 		                            							<div class="col-md-12 col-sm-12 col-xs-12">
+		                            							<div class="x_panel">
 		                            							<div class="x_content">
 				                             						<table id="datatable-responsive{{$a->id}}" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
 		                      												<thead>
@@ -198,6 +199,7 @@
 		                      												<tbody id="datatable-body-bit{{$a->id}}">
 		                          											</tbody>
 		                          									</table>
+				                          						</div>
 				                          						</div>
 				                          						</div>
 				                          						</div>
