bb2ac050b6adb61c448b26ba464ed3fece0ba8fd
ADVANS\mmabel <mmabel@advans.mx>
2017-11-21 15:48:40 -0600

    cambiando clase de tabla bitacora en modal de app

--- cuenta/resources/views/apps.blade.php
+++ cuenta/resources/views/apps.blade.php
@@ -458,12 +458,6 @@
 				            $("#datatable-responsive"+bdid).DataTable( {
 								        responsive: {
 								            details: {
-								                display: $.fn.dataTable.Responsive.display.modal( {
-								                    header: function ( row ) {
-								                        var data = row.data();
-								                        return 'Details for '+data[0]+' '+data[1];
-								                    }
-								                } ),
 								                renderer: $.fn.dataTable.Responsive.renderer.tableAll( {
 								                    tableClass: 'table'
 								                } )
